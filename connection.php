0ab2c8b6d4fe5a549de413f8409b5fcfc0a107cb
Leonardo Barichello <barichello@users.noreply.github.com>
2016-11-29 15:07:08 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "mysql08.mais.mat.br";
-$username = "mais66";
-$password = "pipoca";
-$dbname = "mais66";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 ?>
