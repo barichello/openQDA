00d3fd8c836ca4cfacc72f86c2c6d3da02eb37a2
Leonardo Barichello <barichello@users.noreply.github.com>
2016-12-11 15:01:48 +0000

    Update doRetrieve.php

--- doRetrieve.php
+++ doRetrieve.php
@@ -1,7 +1,5 @@
 <?php
-
 include("connection.php");
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
@@ -9,18 +7,20 @@ if (!$conn) {
     echo "Retrieval failed. ";
     die("Connection failed: " . mysql_error());
 }
-
 $sql = $_POST["string"];
-
 $result = mysqli_query($conn, $sql);
-
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     echo "\n<br>\nNumber os results: " . mysqli_num_rows($result) . "\n<br><br>\n";
     while($row = mysqli_fetch_assoc($result)) {
         //basic info from the sources table
         echo "<div class='item'>";
-        echo "<img src='sources/" . $row["name"]. "'>\n";
+        if ($row["type"]=="i") {
+                echo "<img src='sources/" . $row["name"]. "'>\n";
+                }
+        if ($row["type"]=="v") {
+                echo "<img src='css/video-icon.png'>\n";
+                } 
         echo "<span class='fieldname'>ID: </span><span class='fieldvalue'>" . $row["id"]. "</span><br/><span class='fieldname'>Description: </span><span class='fieldvalue'>" . $row["memo"]. "</span><br/>";
         //getting and showing the attributes
         $result2 = mysqli_query($conn, "SELECT * FROM (SELECT sourceAttributes.value, attributes.name, sourceAttributes.source_id FROM sourceAttributes INNER JOIN attributes ON sourceAttributes.attributes_id=attributes.id)fusao WHERE source_id=" . $row["id"]);
@@ -35,6 +35,5 @@ if (mysqli_num_rows($result) > 0) {
 else {
     echo ":( I could not find any image matching your criterias...";
 }
-
 mysqli_close($conn);
 ?>
