a29ed72404982d7901942317d9a8afe1364d089c
Leonardo Barichello <barichello@users.noreply.github.com>
2017-03-27 13:29:29 +0100

    small improvement

--- doDeleteCoding.php
+++ doDeleteCoding.php
@@ -21,27 +21,30 @@
     
     include("connection.php");
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysql_error());
-}
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysql_error());
+    }
 
-//deleting coding
-$d = str_replace("x"," ",$_GET["date"]);
-$sql = "DELETE FROM sourceCoding WHERE date='" . $d . "'";
+    //deleting coding
+    $d = str_replace("x"," ",$_GET["date"]);
+    $sql = "DELETE FROM sourceCoding WHERE date='" . $d . "'";
 
-if (mysqli_query($conn, $sql)) {
-    echo "<h4>Coding removed<h4>";
-    $last_id = mysqli_insert_id($conn);
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+    if (mysqli_query($conn, $sql)) {
+        echo "<h4>Coding removed.</h4>";
+        $last_id = mysqli_insert_id($conn);
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+        
+    mysqli_close($conn);
     
-mysqli_close($conn);
-?>
-
+    echo "Actions: <a href='viewImagewithBites.php?idImage='" . $_GET["id"] . ">Return to image</a>";
+    
+    ?>
+    
 </div>
 
 </div>
